8bbe200bae4f7f3700ef73aa6810f632a22866b7
omer <you@example.com>
2023-01-27 16:10:26 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,20 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import {trees} from "./parks/RockyMountain"
+import MesaVerde from "./parks/MesaVerde";
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  howManyParks(); // => "42 parks!"
+  
+  return(
+    <div>
+      <MesaVerde />
+      <h1>Colorado State Parks!: {trees}</h1>
+    </div>
+  )
+
 }
+
+export default ColoradoStateParks;
